cc6831b2bd2fa8c0dc3259ca9da742631008f28a
Vishal Pachori <vishalpachori25@gmail.com>
2020-04-11 23:31:10 +0530

    Added categories

--- FCMXamarinFormDemo/FCMXamarinFormDemo.iOS/AppDelegate.cs
+++ FCMXamarinFormDemo/FCMXamarinFormDemo.iOS/AppDelegate.cs
@@ -1,8 +1,11 @@
-﻿using Foundation;
+﻿using Firebase.CloudMessaging;
+using Foundation;
+using ObjCRuntime;
 using Prism;
 using Prism.Ioc;
+using System;
 using UIKit;
-
+using UserNotifications;
 
 namespace FCMXamarinFormDemo.iOS
 {
@@ -10,8 +13,13 @@ namespace FCMXamarinFormDemo.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
+
+        public void DidRefreshRegistrationToken(Messaging messaging, string fcmToken)
+        {
+            System.Diagnostics.Debug.WriteLine( $" FCM Token: { fcmToken } ");
+        }
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -22,9 +30,65 @@ namespace FCMXamarinFormDemo.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Firebase.Core.App.Configure();
             LoadApplication(new App(new iOSInitializer()));
 
+            RegisterToAPNS();
+
             return base.FinishedLaunching(app, options);
+        }
+
+        private void RegisterCategoriesNotification()
+        {
+            var commentID = "comment";
+            var commentTitle = "Comment";
+            var textInputButtonTitle = "Send";
+            var textInputPlaceholder = "Enter comment here...";
+            var commentAction = UNTextInputNotificationAction.FromIdentifier(commentID, commentTitle, UNNotificationActionOptions.None, textInputButtonTitle, textInputPlaceholder);
+
+            // Create category
+            var categoryID = "event-invite";
+            var actions = new UNNotificationAction[] { acceptAction, declineAction, commentAction };
+            var intentIDs = new string[] { };
+            // actions can be construted by UNTextInputNotificationAction class if you want to actions to that 
+            // perticular category
+            var sampleCategory = UNNotificationCategory.FromIdentifier(categoryID, actions, intentIDs, UNNotificationCategoryOptions.None);
+
+            UNUserNotificationCenter.Current.SetNotificationCategories(new NSSet<UNNotificationCategory>(new UNNotificationCategory[] { sampleCategory }));
+        }
+
+        private void RegisterToAPNS()
+        {
+            // Register your app for remote notifications.
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10 or later
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) =>
+                {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                // For iOS 10 data message (sent via FCM)
+                Messaging.SharedInstance.Delegate = this;
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+        }
+
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            base.DidReceiveRemoteNotification(application, userInfo, completionHandler);
         }
     }
 
